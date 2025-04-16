42313620af2f0c0891a4aa825c89b40736e804ae
Jess <shijessica9@gmail.com>
2025-04-16 11:45:33 -0400

    new database created

--- db_models.py
+++ db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Float, DateTime, Integer, UniqueConstraint
+from sqlalchemy import Column, String, Float, DateTime, Integer, UniqueConstraint, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -30,3 +30,16 @@ class Summary(Base):
     sentiment_summary_med = Column(Float)
     sentiment_title_avg = Column(Float)
     sentiment_title_med = Column(Float)
+
+class CompositeScore(Base):
+    __tablename__ = 'composite_scores'
+    
+    id = Column(Integer, primary_key=True)
+    stock = Column(String)
+    date = Column(Date)
+    sentiment = Column(Float)
+    vix = Column(Float)
+    composite_score = Column(Float)
+    
+    def __repr__(self):
+        return f"<CompositeScore(stock='{self.stock}', date={self.date}, score={self.composite_score})>"
