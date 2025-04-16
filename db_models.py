4183b89b9f25680addcb8fddf89909d655d27043
Jess <shijessica9@gmail.com>
2025-04-16 13:29:43 -0400

    fixed deprecated path

--- db_models.py
+++ db_models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Float, DateTime, Integer, UniqueConstraint, Date
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
